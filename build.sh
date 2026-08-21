a718be5d5b4c380fa2a59bf5e5fe06c1bf8ada0c
antonybahati94-lab <antonybahati94@gmail.com>
2026-08-21 12:06:56 +0200

    Create build.sh

---
+++ build.sh
@@ -0,0 +1,10 @@
+#!/usr/bin/env bash
+# exit on error
+set -o errexit
+
+pip install -r requirements.txt
+python manage.py collectstatic --noinput
+python manage.py migrate
+
+# Création du superutilisateur admin
+python manage.py shell -c "from django.contrib.auth import get_user_model; User = get_user_model(); User.objects.filter(username='admin').exists() or User.objects.create_superuser('admin', 'admin@example.com', 'Admin12345!')"
